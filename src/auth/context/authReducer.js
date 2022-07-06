f72e22513a352461cab260d75a9e4ecf4ceb06f6
Alejo Boga <alejoboga20@gmail.com>
2022-07-06 11:56:40 -0300

    AuthContext

---
+++ src/auth/context/authReducer.js
@@ -0,0 +1,19 @@
+import { types } from '../types/types';
+
+const initialState = {
+	logged: false,
+	name: '',
+};
+
+export const authReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case types.login:
+			return { ...state, logged: true, name: action.payload };
+
+		case types.logout:
+			return initialState;
+
+		default:
+			return state;
+	}
+};
