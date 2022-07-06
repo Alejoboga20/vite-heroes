5812067bd3c562694cc61a5e47fb8e4523c0bb5d
Alejo Boga <alejoboga20@gmail.com>
2022-07-06 10:38:34 -0300

    Create search screen

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,3 +1,41 @@
+import { HeroCard } from '../components';
+
 export const SearchPage = () => {
-	return <div>SearchPage</div>;
+	return (
+		<>
+			<h1>SearchPage</h1>
+			<hr />
+
+			<div className='row'>
+				<div className='col-5'>
+					<h4>Searching</h4>
+					<hr />
+
+					<form>
+						<input
+							type='text'
+							placeholder='search hero'
+							className='form-control'
+							name='searchText'
+							autoComplete='off'
+						/>
+					</form>
+
+					<button className='btn btn-outline-primary mt-2'>Search</button>
+				</div>
+
+				<div className='col-7'>
+					<h4>Results</h4>
+					<hr />
+
+					<div className='alert alert-primary'>Search a hero</div>
+					<div className='alert alert-danger'>
+						There's no result with <b>ABC</b>
+					</div>
+
+					{/* <HeroCard /> */}
+				</div>
+			</div>
+		</>
+	);
 };
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
 	return (
 		<nav className='navbar navbar-expand-sm navbar-dark bg-dark p-2'>
 			<Link className='navbar-brand' to='/'>
-				Asociaciones
+				Associations
 			</Link>
 
 			<div className='navbar-collapse'>
@@ -27,6 +27,13 @@ export const Navbar = () => {
 						to='/dc'
 					>
 						DC
+					</NavLink>
+
+					<NavLink
+						className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+						to='/search'
+					>
+						Search
 					</NavLink>
 				</div>
 			</div>
