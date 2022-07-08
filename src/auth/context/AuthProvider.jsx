14a283863a50ba5760becd360857939e89a915da
Alejo Boga <alejoboga20@gmail.com>
2022-07-08 16:39:20 -0300

    Login user

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from 'react';
+import { types } from '../types/types';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 
@@ -10,5 +11,14 @@ const initialState = {
 export const AuthProvider = ({ children }) => {
 	const [authState, dispatch] = useReducer(authReducer, initialState);
 
-	return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+	const login = (name = '') => {
+		const loginAction = {
+			type: types.login,
+			payload: name,
+		};
+
+		dispatch(loginAction);
+	};
+
+	return <AuthContext.Provider value={{ ...authState, login }}>{children}</AuthContext.Provider>;
 };
