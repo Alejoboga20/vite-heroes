e1f05fac22a5b8e551fb28585152161e9015c827
Alejo Boga <alejoboga20@gmail.com>
2022-07-09 11:54:25 -0300

    Persist user

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,19 +3,27 @@ import { types } from '../types/types';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 
-const initialState = {
-	logged: false,
-	name: '',
+const init = () => {
+	const user = JSON.parse(localStorage.getItem('user'));
+
+	return {
+		logged: !!user,
+		user,
+	};
 };
 
 export const AuthProvider = ({ children }) => {
-	const [authState, dispatch] = useReducer(authReducer, initialState);
+	const [authState, dispatch] = useReducer(authReducer, {}, init);
 
 	const login = (name = '') => {
+		const user = { ud: 'ABC', name };
+
 		const loginAction = {
 			type: types.login,
 			payload: name,
 		};
+
+		localStorage.setItem('user', JSON.stringify(user));
 
 		dispatch(loginAction);
 	};
