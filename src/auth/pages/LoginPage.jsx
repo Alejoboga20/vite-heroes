81d0c3222dbe612b6cd1371d5d84e6fd66ef11d2
Alejo Boga <alejoboga20@gmail.com>
2022-07-09 12:25:11 -0300

    Record last path

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -7,8 +7,10 @@ export const LoginPage = () => {
 	const navigate = useNavigate();
 
 	const onLogin = () => {
+		const lastPath = localStorage.getItem('lastPath') || '/';
+
 		login('Alejo ');
-		navigate('/', { replace: true });
+		navigate(lastPath, { replace: true });
 	};
 
 	return (
