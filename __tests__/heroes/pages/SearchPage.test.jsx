14c3017a53ef072ede53ec225c0dc41c7123de1e
Alejo Boga <alejoboga20@gmail.com>
2022-07-11 13:01:01 -0300

    Test search page

--- __tests__/heroes/pages/SearchPage.test.jsx
+++ __tests__/heroes/pages/SearchPage.test.jsx
@@ -1,6 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter, useNavigate } from 'react-router-dom';
 import { SearchPage } from '../../../src/heroes/pages/SearchPage';
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+	...jest.requireActual('react-router-dom'),
+	useNavigate: () => mockedUseNavigate,
+}));
 
 describe('SearchPage Tests', () => {
 	test('should match Snapshot', () => {
@@ -28,5 +35,31 @@ describe('SearchPage Tests', () => {
 
 		const alert = screen.getByLabelText('alert-danger');
 		expect(alert.style.display).toContain('none');
+	});
+
+	test('should show error when no hero is found', () => {
+		render(
+			<MemoryRouter initialEntries={['/search?q=noherowiththisname']}>
+				<SearchPage />
+			</MemoryRouter>
+		);
+
+		expect(screen.getByText("There's no result with")).toBeTruthy();
+	});
+
+	test('should call navigate to new screen', () => {
+		render(
+			<MemoryRouter initialEntries={['/search']}>
+				<SearchPage />
+			</MemoryRouter>
+		);
+
+		const input = screen.getByRole('textbox');
+		fireEvent.input(input, { target: { value: 'batman' } });
+
+		const submitButton = screen.getByRole('button', { name: 'Search' });
+		fireEvent.click(submitButton);
+
+		expect(mockedUseNavigate).toHaveBeenCalledWith('?q=batman');
 	});
 });
